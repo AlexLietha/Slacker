43cff8ae709397e1c0aea7809c9708de45011f54
Aidan <aidanmusil@gmail.com>
2026-09-26 00:33:03 -0500

    Attempt at multiplayer
    - Multiplayer kinda works
    -Also removed some old unused code (player_controller.gd)
    -Also also added comments in playerMovement.gd

--- slacker/Scripts/playerMovement.gd
+++ slacker/Scripts/playerMovement.gd
@@ -1,18 +1,22 @@
 extends CharacterBody3D
 class_name Player
 
+# Player settings
 @export var speed := 5.0
 @export var jumpVelocity := 5
 @export var mouseSensitivity := 0.004
-@export var role = "Cook"
 
+# Player "parts"
 @onready var camera = $Camera3D
 @onready var hat := $Hat
 @onready var player_mesh := $PlayerMesh
 @onready var vote_UI := $VotingBoard
-
 @export var interactionRayCast : RayCast3D
+
 @export var grabbedItem : Node3D
+
+# Player data
+var role = "Cook"
 var counter: int = 0;
 
 func _ready() -> void:
@@ -20,10 +24,11 @@ func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	vote_UI.visible = false
 	
-	
+func _enter_tree() -> void:
+	set_multiplayer_authority(name.to_int())
 
 func _physics_process(delta: float) -> void:
-	
+	if !is_multiplayer_authority(): return
 		
 	if role == "Manager":
 		hat.show()
@@ -50,27 +55,20 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("jump") and is_on_floor():
-		#velocity.y = jumpVelocity
-
 	var input_dir := Input.get_vector("moveLeft", "moveRight", "moveForward", "moveBack")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-
 		velocity.x = direction.x * speed
 		velocity.z = direction.z * speed
-		
 	else:
-		
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 
-	if(Input.is_action_just_pressed("Click e")&&counter%2==0):
-		ShowUI()
-		counter = counter +1
-	elif(Input.is_action_just_pressed("Click e")&&counter%2==1):
-		HideUI()
+	if(Input.is_action_just_pressed("clickE")&&counter%2==0):
+		show_vote_ui()
+		counter = counter + 1
+	elif(Input.is_action_just_pressed("clickE")&&counter%2==1):
+		hide_vote_ui()
 		counter= counter + 1
 
 	move_and_slide()
@@ -103,11 +101,11 @@ func getGrabbedItem() -> GrabbableObject:
 func HasGrabbedItem() -> bool:
 	return grabbedItem.get_child_count() == 1
 	
-func ShowUI() -> void:
+func show_vote_ui() -> void:
 	vote_UI.visible = true
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
-func HideUI() -> void:
+func hide_vote_ui() -> void:
 	vote_UI.visible = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)	
 
